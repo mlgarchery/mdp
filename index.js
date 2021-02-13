0ac062ad383cfaa3ae2d77e4182160432c90f4d8
Martial Garchery <martial.garchery@gmail.com>
2021-02-13 19:07:31 +0100

    jekyll test

---
+++ index.js
@@ -0,0 +1,11 @@
+import { alist } from "./src/module.js";
+
+console.log(alist);
+const salut = "slkdls";
+let liste = ["lsk", "lskdl", "slkdl"];
+liste.forEach(el => console.log(el));
+
+console.log(salut);
+
+
+export default salut;
